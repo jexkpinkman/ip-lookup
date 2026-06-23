fdc79317862712ab0062cf4adbf5c1085c26ef24
Jexkkk_ <jexkpinkman@gmail.com>
2026-06-24 03:02:42 +0700

    Update route.ts

--- app/api/lookup/route.ts
+++ app/api/lookup/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { IPData, LookupResult } from "@/types";
 
-export const runtime = "edge";
-
 export async function GET(request: NextRequest): Promise<NextResponse<LookupResult>> {
   const { searchParams } = new URL(request.url);
   const ip = searchParams.get("ip");
